507338c35c9bd6aab264249119a1fcc2035fd482
Jessica Almand <jlalmand27@gmail.com>
2022-11-25 12:38:42 -0500

    Added option to connect with server, made styling changes

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const express = require('express');
+// const path = require('path');
+// const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-const app = express();
+// const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+// // GET Route for homepage
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+// app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
